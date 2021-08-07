5f3816830f55ab1e156232c77617fd6f22266934
Shesha234 <60121377+Shesha234@users.noreply.github.com>
2021-08-07 21:27:45 +0530

    login

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ if(empty($err))
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
   <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="index.html">Home</a> <!--<span class="sr-only">(current)</span></a>-->
       </li>
       <li class="nav-item">
         <a class="nav-link" href="register.php">Register</a>
